5cce7161c8cc7dff3ccaaea2ea9d0f57b95f638a
OhMinSeop <mins5190@gmail.com>
2023-09-06 18:09:19 +0900

    add: tiptab 컴포넌트 구성중

---
+++ app/components/shared/future/Tiptap/index.ts
@@ -0,0 +1,1 @@
+export { TipTapEditor } from "./TipTapEditor";
--- app/components/write/future/WriteContent/WriteContent.tsx
+++ app/components/write/future/WriteContent/WriteContent.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./styles.module.css";
+import { TipTapEditor } from "~/components/shared/future/Tiptap";
 
 export default function WriteConrent() {
   return (
-    <div className={styles.styles}>
-      <p style={{ color: "black", height: "1024px" }}>
-        WriteConrentasdasdsasdsa
-      </p>
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <TipTapEditor />
+        </div>
+      </div>
     </div>
   );
 }
