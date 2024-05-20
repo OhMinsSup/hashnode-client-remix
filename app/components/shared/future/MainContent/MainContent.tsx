8d88b9a100365b6c64dd1bbc6eaa2c212be54229
OhMinSeop <mins5190@gmail.com>
2024-05-21 01:59:08 +0900

    add: 메인 aside 영역 추가

--- app/components/shared/future/MainContent/MainContent.tsx
+++ app/components/shared/future/MainContent/MainContent.tsx
@@ -10,7 +10,12 @@ export default function MainContent({ children, aside }: MainContentProps) {
   return (
     <>
       <div className={cn(styles.root)}>{children}</div>
-      <aside className={cn(styles.aside)}>{aside}</aside>
+      <aside
+        className={cn(styles.aside)}
+        style={{ top: 0, position: 'relative', height: '100%' }}
+      >
+        {aside}
+      </aside>
     </>
   );
 }
